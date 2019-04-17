0cdb276f524c6b7a454f32ac68896968cd813c79
Manu Nelamane Siddalingegowda <manu@lightcurve.io>
2019-04-17 18:23:44 +0200

    :seedling: Implement node endpoint

---
+++ src/api/node.rs
@@ -0,0 +1,13 @@
+use api::models::{NodeStatus};
+use api::Result;
+use http::client::Client;
+
+pub struct Node {
+    client: Client,
+}
+
+impl Node {
+    pub fn new(client: Client) -> Node { Node { client } }
+
+    pub fn status(&self) -> Result<NodeStatus> { self.client.get("node/status") }
+}
